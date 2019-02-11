9e78db6434ce6d4d4c4a335d881f45cdacecbeed
Kamal Mukherjee <kamalnyc17@gmail.com>
2019-02-11 09:58:06 -0500

    Totally Trivial Trivia

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+var gameName = [4];
+
+
+/* initializing array */
+gameName[0] = "What was the first hit of Michael Jackson?";
+gameName[1] = "What was the first movie of whitney houston?";
+gameName[2] = "What is the name of the song with David Bowie & Mick Jagger?";
+gameName[3] = "When was the song Hotel California released?";
+
+
+
