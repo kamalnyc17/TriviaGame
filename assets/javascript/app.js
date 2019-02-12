66cb62cc46d9f0e38f842bf86ac7b6a2db92137b
Kamal Mukherjee <kamalnyc17@gmail.com>
2019-02-12 09:53:53 -0500

    Totally Trivial Trivia

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,26 @@
 var gameName = [];
 var i;
 
-/* initializing array */
-gameName[0] = ["What was the first hit of Michael Jackson?", "AAA1", "AAA2", "AAA3", "AAA4"];
-gameName[1] = ["What was the first movie of whitney houston?", "BBB1", "BBB2", "BBB3", "BBB4"];
-gameName[2] = ["What is the name of the song with David Bowie & Mick Jagger?", "CCC1", "CCC2", "CCC3", "CCC4"];
-gameName[3] = ["When was the song Hotel California released?", "DDD1", "DDD2", "DDD3", "DDD4"];
+/* initializing array. 1st element is question. last element is the correct answer.  */
+gameName[0] = ["What was the first hit of Michael Jackson?", "AAA1", "AAA2", "AAA3", "AAA4", "AAA1"];
+gameName[1] = ["What was the first movie of whitney houston?", "BBB1", "BBB2", "BBB3", "BBB4", "BBB2"];
+gameName[2] = ["What is the name of the song with David Bowie & Mick Jagger?", "CCC1", "CCC2", "CCC3", "CCC4", "CCC3"];
+gameName[3] = ["When was the song Hotel California released?", "DDD1", "DDD2", "DDD3", "DDD4", "DDD4"];
 
-// game work flow starts here after the whole page is loaded
+// start the game with only <start> button visible
+var startGame = function() {
+    $(".gen-style, .sub-style, img").hide();
+} 
+
+// main loop
+$(document).ready(function() {
+    startGame();
+
+    
+});
+
+
+/* game work flow starts here after the whole page is loaded
 $(document).ready(function () {
 
     for (i = 0; i < 4; i++) {
@@ -27,3 +40,4 @@ $(document).ready(function () {
         $("#winner").show();
     });
 });
+*/
